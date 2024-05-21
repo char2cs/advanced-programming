c03e4a457b457c5d25f2444d25b0f546623f506b
Mateo Urrutia <mateourru@gmail.com>
2024-05-21 19:14:11 -0300

    Linked projects!

--- GestionEscolarSwing/src/main/java/mateourrutia/Main.java
+++ GestionEscolarSwing/src/main/java/mateourrutia/Main.java
@@ -1,7 +1,24 @@
 package mateourrutia;
 
+import mateourrutia.DAO.imp.ProfesorDAOImp;
+import mateourrutia.domain.Profesor;
+
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
+		ProfesorDAOImp profesorDAO = new ProfesorDAOImp();
 
+		profesorDAO.add(new Profesor(
+				0,
+				45518340,
+				"Mateo",
+				"Urrutia",
+				"test",
+				new ArrayList<>()
+		));
+
+		for (Profesor profesor : profesorDAO.getAll())
+			System.out.println(profesor);
 	}
 }
